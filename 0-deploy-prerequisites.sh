ccb9fa331e4505b3ee8b2c62c0885d4b3602cd74
Paul Wilkinson <pmw@redpanda.com>
2024-08-30 15:18:43 +0100

    Adding in RPCN repo step.

--- 0-deploy-prerequisites.sh
+++ 0-deploy-prerequisites.sh
@@ -13,3 +13,10 @@ echo "+-----------------------+"
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install prometheus prometheus-community/prometheus
+
+echo "+-----------------------------------------+"
+echo "| Adding Redpanda Connect Helm Chart Repo |"
+echo "+-----------------------------------------+"
+
+helm repo add benthos https://benthosdev.github.io/charts/
+helm repo update
